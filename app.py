4dd4b66461fded70c21f08b99161d17e9e6622eb
Srirama Bhat <sriramarbhat@gmail.com>
2024-03-20 16:33:07 +0530

    Added template model loading

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
+import tensorflow as tf
+import numpy as np
+import transformers
+import tensorflow_text as text
+import os
 
 UPLOAD_FOLDER = "./static"
 ALLOWED_EXTENSIONS = ["txt", "docx", "pdf", "jpeg", "jpg", "png", "mp3", "wav", "ogg"]
@@ -10,6 +14,11 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route("/predict", methods=["POST"])
+def load_and_predict_text(text):
+  text_model = tf.keras.models.load_model("./models/bert_model (1)")
+  return text_model
+
+@app.route("/predict", methods=["GET"])
 def predict_deepfake():
-  return 0
+  model = load_and_predict_text("txt")
+  return model.summary()
