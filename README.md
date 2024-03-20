d2c461cdd3dd943e2e8726d207c944a8ded07622
Srirama Bhat <sriramarbhat@gmail.com>
2024-03-20 15:31:02 +0530

    Added template backend with final file structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# Deep_Fake_Detection_Backend
-Repository for storing the models and backend routing for the Deepfake detection project. 
--- app.py
+++ app.py
@@ -1,52 +1,15 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-import flask_cors
-import pickle
-# import logging
+from flask_cors import CORS
 
-# logging.basicConfig(level=logging.INFO)
-
-# logger = logging.getLogger('HELLO WORLD')
-
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'docx', 'png', 'jpg', 'jpeg', 'mp3', 'wav', 'ogg'])
-TEXT_EXTENSIONS = set(["txt", "pdf", "docx"])
-IMAGE_EXTENSIONS = set(["png", "jpeg", "jpg"])
-AUDIO_EXTENSIONS = set(["mp3", "wav", "ogg"])
+UPLOAD_FOLDER = "./static"
+ALLOWED_EXTENSIONS = ["txt", "docx", "pdf", "jpeg", "jpg", "png", "mp3", "wav", "ogg"]
 
 app = Flask(__name__)
-flask_cors.CORS(app)
+cors = CORS(app)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/predict', methods=['POST'])
-def fileUpload():
-    file = request.files.get("file")
-    extension = file.filename.split(".")[1].strip()
-    destination="./upload_files/"
-    file.save(destination+file.filename)
-    if extension not in ALLOWED_EXTENSIONS:
-      data = {
-        "status_code": 400,
-        "probability": -1,
-        "message": "File extension not supported"
-      }
-      return jsonify(data)
-    else:
-      model_path = "/models"
-      if extension in TEXT_EXTENSIONS:
-        model_path += "/text_model.pkl"
-      elif extension in IMAGE_EXTENSIONS:
-        model_path += "/image_model.pkl"
-      else:
-        model_path += "/audio_model.pkl"
-      model = pickle.load(model_path)
-      probability = model.predict(destination)
-      data = {
-        "status_code": 201,
-        "probability": probability,
-        "message": "success"
-      }
-      return jsonify(data)
-
-if __name__ == "__main__":
-    app.run()
-
+@app.route("/predict", methods=["POST"])
+def predict_deepfake():
+  return 0
